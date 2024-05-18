876a4082f421e06316c384d0ea0af2c496273e3b
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-05-17 22:48:34 -0400

    added media queries

--- ageCaluclator/src/Components/Rectangle1.tsx
+++ ageCaluclator/src/Components/Rectangle1.tsx
@@ -11,20 +11,20 @@ const Rectangle1 = ({day,setDay, month,setMonth, year,setYear}) => {
     <form  >
       <div className="box">
         <label>DAY</label>
-        <div className="input-container">
-          <input type="number" placeholder="00" min='1'  max='31' value={day} onChange={(e) =>{setDay(Number(e.target.value))}} />
+        <div className="input-containerr">
+          <input type="number" placeholder="DD" maxLength={2} min='1'  max='31' value={day} onChange={(e) =>{setDay(Number(e.target.value))}} />
         </div>
       </div>
       <div className="box">
         <label>MONTH</label>
-        <div className="input-container">
-          <input type="number" placeholder="00"   min='1' max='12' value={month} onChange={(e) =>{setMonth(Number(e.target.value))}} />
+        <div className="input-containerr">
+          <input type="number" placeholder="MM"   min='1' max='12' value={month} onChange={(e) =>{setMonth(Number(e.target.value))}} />
         </div>
       </div>
       <div className="box">
         <label>YEAR</label>
-        <div className="input-container">
-          <input type="number" placeholder="1994" min='1920' max='2024'  value={year} onChange={(e) =>{setYear(Number(e.target.value))}}/>
+        <div className="input-containerr">
+          <input type="number" placeholder="YYYY" min='1920' max='2024'  value={year} onChange={(e) =>{setYear(Number(e.target.value))}}/>
         </div>
       </div>
       
